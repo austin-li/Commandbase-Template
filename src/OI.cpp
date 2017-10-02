77ad6cace8dda8a62a63b68094a2661dab49c37c
LAPTOP-A4KE1I09\Austin <austinmingli@gmail.com>
2017-10-01 17:14:56 -0700

    creates left and right Joysticks

--- src/OI.cpp
+++ src/OI.cpp
@@ -2,6 +2,14 @@
 
 #include <WPILib.h>
 
-OI::OI() {
+OI::OI() :leftStick(new Joystick(1)), rightStick(new Joystick(2)){
 	// Process operator interface input here.
+}
+
+Joystick* OI::getLeft() {
+	return leftStick;
+}
+
+Joystick* OI::getRight() {
+	return rightStick;
 }
--- src/OI.h
+++ src/OI.h
@@ -1,9 +1,16 @@
 #ifndef OI_H
 #define OI_H
 
+#include "WPILib.h"
+
 class OI {
+private:
+	Joystick* leftStick;
+	Joystick* rightStick;
 public:
 	OI();
+	Joystick* getLeft();
+	Joystick* getRight();
 };
 
 #endif  // OI_H
